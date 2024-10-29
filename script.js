c4dcb997af94fca81095edebd571c10e49ee2d01
copilot7 <azizqatar24@gmail.com>
2024-10-29 12:46:49 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+﻿
+    var alerts = [
+        "⚠️ Warning: Your system has been hacked!",
+        "⚠️ Virus is loading...",
+        "⚠️ All your files have been destroyed!",
+        "😈 Ha ha ha! its just a prank! Don't worry !!"
+    ];
+
+    function showScaryContent() {
+        modal.style.display = 'none';
+        image.classList.remove('hidden');
+        alertContainer.classList.remove('hidden');
+       
+        let delay = 0;
+        alerts.forEach((message, index) => {
+            setTimeout(() => {
+                var alertBox = document.createElement('div');
+                alertBox.classList.add('alert-box');
+                alertBox.innerText = message;
+                alertContainer.insertBefore(alertBox, alertContainer.firstChild);
+                var audio = new Audio('notification.mp3'); // تأكد من وجود ملف الصوت في نفس المجلد
+                audio.play();
+            }, delay);
+            delay += 2000;
+        });
+    }
+
+    acceptBtn.onclick = showScaryContent;
+    declineBtn.onclick = showScaryContent;
+};
